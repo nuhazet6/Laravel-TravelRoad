061e557b59990d825b75c8dc5d3da98d32d5b85e
nuhazet6 <nuhazet6@gmail.com>
2023-12-17 02:20:15 +0000

    Aplicación completada

---
+++ travelroad/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+  $wished = DB::select('select * from places where visited = false');
+  $visited = DB::select('select * from places where visited = true');
+
+  return view('travelroad', ['wished' => $wished, 'visited' => $visited]);
+});
